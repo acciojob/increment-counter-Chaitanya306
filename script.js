e744d8fbb20604baf969c78ac37f978f4281c0f6
Chaitanya306 <98307330+Chaitanya306@users.noreply.github.com>
2025-03-27 14:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const count=document.getElementById("count")
+const btn=document.getElementById("btn")
+
+function increment(){
+	const cnt=Number(count.textContent)
+	cnt++
+	count.textContent=cnt
+}
